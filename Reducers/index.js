0100ec23500b0827bb0879b2452f719be0b081ec
mnfgul <mnfgul@users.noreply.github.com>
2017-06-12 12:41:51 +0300

    Create index.js

--- Reducers/index.js
+++ Reducers/index.js
@@ -8,7 +8,4 @@ export default combineReducers({
   event: EventReducer
 })
 
-// Put reducer keys that you do NOT want stored to persistence here
 export const persistentStoreBlacklist = ['login']
-// OR put reducer keys that you DO want stored to persistence here (overrides blacklist)
-// export const persistentStoreWhitelist = []
